18f7c794d83fc7cc1eeefd5571abb353160d2ae0
Luz Gutierrez <mili01gm@gmail.com>
2017-03-01 10:29:44 -0500

    html y js listos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+var peso = document.getElementById("peso");
+var altura = document.getElementById("altura");
+var result = document.getElementById('result');
+var msj = document.getElementById('msj');
+
 function calcImc(){
-  
+  var formula = peso.value/(altura.value*altura.value);
+  var respuesta =formula.toFixed(2);
+
+  //console.log(respuesta);
+  result.value = respuesta;
+
 }
